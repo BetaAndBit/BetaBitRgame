39d730d90fb1d3cdc47dbd98f5ee32f0f4f351bd
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-07-25 19:28:51 +0200

    man pages for datasets in regression()

---
+++ R/dataFSW.R
@@ -0,0 +1,21 @@
+#' The data from the study of Polish upper-secondary schools students.
+#'
+#' It was conducted firstly in the same time as PISA 2009 study, with use of the
+#' same cognitive tests and questionnaires as in PISA 2009, but on a different
+#' group of students: first grade students of upper-secondary schools (in Poland
+#' most of the students in a regular PISA sample attends lower-secondary
+#' schools). The students who participated in the first wave of the study were
+#' followed in the 2nd grade of upper-secondary school within the research
+#' program \emph{Our further study and work} (\emph{Nasza Dalsza Nauka i Praca}).
+#' Both studies were conducted by the Institute of Philosophy and Sociology
+#' Polish Academy of Sciences.
+#'
+#' \strong{The original data was changed a little, to better fit the purpose of
+#' the game.}
+#'
+#' @docType data
+#' @keywords datasets
+#' @name dataFSW
+#' @format data frame: 3796 obs. of 54 variables
+NULL
+
---
+++ R/varLabels.R
@@ -0,0 +1,8 @@
+#' The data frame containng labels of the variables from \code{dataDNiP} and
+#' \code{DNiP} datasets.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name varLabels
+#' @format data frame: 54 obs. of 2 variables
+NULL
